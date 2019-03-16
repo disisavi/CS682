8d19b3ee288d5855bffbdde5e1739d42953dd361
disisavi <disisavi@github.com>
2019-03-16 03:47:03 -0400

    Color Gradient and intersection added

--- ASST3/HW3Code/asst3.py
+++ ASST3/HW3Code/asst3.py
@@ -19,16 +19,21 @@ def convertToGray(images):
 
 def imageGradientGray(images):
     detected_edges = [cannyEdgeDetector(image) for image in images]
-    returnList = [sobelList(edge) for edge in detected_edges]
+    returnList = []
+    for edge in detected_edges:
+        a = sobelList(edge)
+        angle = cv2.phase(a[0], a[1], angleInDegrees=True)
+        angle = angle // 10
+        hist = np.histogram(angle, 35, [0, 35])
+        returnList.append(hist[0])
     return returnList
 
+
 def sobelList(edge):
-    
-    tempList = []
-    tempList.append(cv2.Sobel(edge,cv2.CV_64F,1,0,ksize=5))
-    tempList.append(cv2.Sobel(edge,cv2.CV_64F,0,1,ksize=5))
+    tempList = [cv2.Sobel(edge, cv2.CV_64F, 1, 0, ksize=5), cv2.Sobel(edge, cv2.CV_64F, 0, 1, ksize=5)]
 
     return tempList
+
 
 def cannyEdgeDetector(image):
     sigma = 0.6
@@ -41,23 +46,31 @@ def cannyEdgeDetector(image):
 
 
 def cannyEdgesColor(images):
-    blurImages = []
+    hist = []
+
     for image in images:
-        edge = np.copy(image)
-        edge[:, :, 0] = cannyEdgeDetector(image[:, :, 0])
-        sobelListBlue = sobelList(edge[:, :, 0])
-        edge[:, :, 1] = cannyEdgeDetector(image[:, :, 1])
-        sobelListGreen = sobelList(edge[:, :, 1])
-        edge[:, :, 2] = cannyEdgeDetector(image[:, :, 2])
-        sobelListRed = sobelList(edge[:, :, 2])
-        blurImages.append([sobelListRed,sobelListGreen,sobelListBlue])
+        rEdge = cannyEdgeDetector(image[:, :, 2])
+        gEdge = cannyEdgeDetector(image[:, :, 1])
+        bEdge = cannyEdgeDetector(image[:, :, 0])
+        r = sobelList(rEdge)
+        b = sobelList(bEdge)
+        g = sobelList(gEdge)
 
-    return blurImages
+        x = b[0] + g[0] + r[0]
+        y = b[1] + g[1] + r[1]
 
-def grayEdgeHistogram(grayEdges):
-    angle = [cv2.phase(grayEdges[i][0],grayEdges[i][1],angleInDegrees=True) for i in range(len(grayEdges)) ]
-    for i in range(len(angle)):
-        print(np.amax(angle[i]))
+        m, angle = cv2.cartToPolar(x, y, angleInDegrees=True)
+        angle = angle // 10
+        histogram = np.histogram(angle, range(36), weights=m)
+        hist.append(histogram[0])
+    return hist
+
+
+def histogram_intersection(hist1, hist2):
+    min = np.sum(np.minimum(hist1, hist2))
+    max = np.sum(np.maximum(hist1, hist2))
+    return min / max
+
 
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -69,14 +82,36 @@ def main():
     grayImages = convertToGray(images)
     print("Gray image conversion done")
     print("Taking image Gradient of Gray images")
-    grayEdges = imageGradientGray(grayImages)
+    grayHistogramList = imageGradientGray(grayImages)
     print("gray image gradient calculation done")
-    print("Taking image Gradient of Color images")
-    colorEdges = cannyEdgesColor(images)
-    print("Color image gradient calculation done")
-    print("Gray edge histogram calculation begins")
-    grayEdgeHistogram(grayEdges)
+    histogram_intersection_matrix = np.zeros((99, 99), dtype=np.int16)
+    for i in range(len(grayHistogramList)):
+        for j in range(len(grayHistogramList)):
+            histogram_intersection_matrix[i][j] = (histogram_intersection(grayHistogramList[i],
+                                                                          grayHistogramList[j])) * 255
 
+    histogram_intersection_matrix = histogram_intersection_matrix - 255
+    histogram_intersection_matrix = np.absolute(histogram_intersection_matrix)
+    print("Intersection matrix after scaling \n", histogram_intersection_matrix)
+    plt.imshow(images[0])
+    plt.colorbar()
+    plt.title("Intersectoin Matrix")
+    plt.show()
+
+    colorHistogramList = cannyEdgesColor(images)
+    histogram_intersection_matrix = np.zeros((99, 99), dtype=np.int16)
+    for i in range(len(colorHistogramList)):
+        for j in range(len(colorHistogramList)):
+            histogram_intersection_matrix[i][j] = (histogram_intersection(colorHistogramList[i],
+                                                                          colorHistogramList[j])) * 255
+
+    histogram_intersection_matrix = histogram_intersection_matrix - 255
+    histogram_intersection_matrix = np.absolute(histogram_intersection_matrix)
+    print("Intersection matrix after scaling \n", histogram_intersection_matrix)
+    plt.imshow(histogram_intersection_matrix)
+    plt.colorbar()
+    plt.title("Intersection Matrix")
+    plt.show()
 
 
 if __name__ == "__main__": main()
