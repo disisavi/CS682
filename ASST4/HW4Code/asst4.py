60652b9f0cc40b7ce75ff2bc9de61c76449b04b1
Abhijeet Mishra <dsisavi@gmail.com>
2019-04-23 18:33:54 -0400

    1. Line drawing functionality added and 2. Thersholding added

--- ASST4/HW4Code/asst4.py
+++ ASST4/HW4Code/asst4.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import math
 import glob
 import os
+import copy
 
 location = '../ImageSource/ST2MainHall4/'
-alphas = [61.9999, 0, 45]
+alphas = [0, 90, 45]
 
 
 def edgeImage(image):
@@ -26,25 +27,37 @@ def getanges(alpha):
     return sine, cosine
 
 
-def printlines(image, rho, theta):
-    a = np.cos(theta)
-    b = np.sin(theta)
+def printlines(image, rho, theta, inversebit=False):
+    radian = 0
+    if inversebit:
+        radian = math.radians(theta)
+    else:
+        radian = theta
+
+    a = math.cos(radian)
+    b = math.sin(radian)
+
     x0 = a * rho
     y0 = b * rho
     x1 = int(x0 + 1000 * (-b))
     y1 = int(y0 + 1000 * (a))
     x2 = int(x0 - 1000 * (-b))
     y2 = int(y0 - 1000 * (a))
-    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    if inversebit:
+        cv2.line(image, (y1, x1), (y2, x2), (0, 0, 255), 2)
+    else:
+        cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
 
-def getLinePoints(dvalue, image, count):
+def getLinePoints(dvalueMatrix, image, count):
     image = image.astype(int)
     image[image == 254] = -1
     image[image > 0] = 75
     image[image == -1] = 254
+    dValue = []
     frequency = {}
-    for (x, y), value in np.ndenumerate(dvalue):
+    for (x, y), value in np.ndenumerate(dvalueMatrix):
         if value != 0:
             frequency.setdefault(value, []).append((x, y))
 
@@ -54,35 +67,34 @@ def getLinePoints(dvalue, image, count):
 
     for i, (k, v) in enumerate(frequencyDesc.items()):
         # print("\t- > ",frequency[k])
-        idx = np.array(frequency[k])
-        image[idx[:, 0], idx[:, 1]] = 254
-        if i > count:
+        if (frequencyDesc[k] > 200):
+            # print(k, frequencyDesc[k])
+            idx = np.array(frequency[k])
+            image[idx[:, 0], idx[:, 1]] = 254
+            dValue.append(k)
+            if i > count:
+                break
+        else:
             break
-    return image
+    if not dValue:
+        return image, None
+    return image, dValue
 
 
-def radonTransform(image, alpha):
-    height, width = image.shape
+def radonTransform(edgeImage, alpha, image):
+    image = copy.copy(image)
+    height, width = edgeImage.shape
     returnImage = np.zeros((height, width))
-    indices = np.nonzero(image)
+    indices = np.nonzero(edgeImage)
     sine, cosine = getanges(alpha)
     returnImage[indices] = indices[0] * cosine + indices[1] * sine
-
-    return getLinePoints(returnImage, image, 2)
-
-
-def radonTransformpy(image, alpha):
-    #     print(image.shape)
-    height, width = image.shape
-    returnImage = np.zeros((height, width))
-    sine, cosine = getanges(alpha)
-    for (x, y), item in np.ndenumerate(image):
-        if item > 0:
-            #     print("{"+str(x)+","+str(y)+"} --> "+str(x*cosine))
-            returnImage[x][y] = x * cosine + y * sine
-    #     print(np.amax(returnImage), np.amin(returnImage))
-
-    return getLinePoints(returnImage, image, 2)
+    hImage, dlist = getLinePoints(returnImage, edgeImage, 2)
+    if dlist is not None:
+        for d in dlist:
+            printlines(image, d, alpha, True)
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.show()
+    return hImage
 
 
 def sobelList(edge):
@@ -117,7 +129,7 @@ def imageGradientGray(images):
         for element in max:
             angle = bins[hist.index(element)] * 5
             print(angle)
-            e = radonTransform(e, angle)
+            e = radonTransform(e, angle,image)
         plt.plot(hist, color="black")
         plt.show()
         returnList.append(e)
@@ -128,7 +140,7 @@ def imageGradientGray(images):
 def getHoughTransform(image):
     edge = edgeImage(image)
     height, width = edge.shape
-    lines = cv2.HoughLines(edge, 1, np.pi / 180,(int)((height+width)/9) )
+    lines = cv2.HoughLines(edge, 1, np.pi / 180, (int)((height + width) / 9))
     if lines is not None:
         for line in lines:
             for rho, theta in line:
@@ -151,30 +163,30 @@ def loadALlImages(location):
 # Implementaion starts from here 
 os.system('cls' if os.name == 'nt' else 'clear')
 images = loadALlImages(location)
-# print("image, angle")
-# for i, image in enumerate(images):
-#     edge = edgeImage(image)
-#     # plt.imshow(edge, cmap="gray")
-#     # plt.show()
-#     for alpha in alphas:
-#         print(i, '\t', alpha)
-#         transform = radonTransform(edge, alpha)
-#         plt.imshow(transform, cmap='bone')
-#         plt.colorbar()
-#         plt.show()
-# grayimages = [cv2.cvtColor(image, cv2.cv2.COLOR_BGR2GRAY) for image in images]
-# lineImages = imageGradientGray(grayimages)
-# for image in lineImages:
-#     plt.imshow(image, cmap='bone')
-#     plt.colorbar()
-#     plt.show()
+print("image, angle")
+for i, image in enumerate(images):
+    edge = edgeImage(image)
+    # plt.imshow(edge, cmap="gray")
+    # plt.show()
+    for alpha in alphas:
+        print(i, '\t', alpha)
+        transform = radonTransform(edge, alpha, image)
+        plt.imshow(transform, cmap='bone')
+        plt.colorbar()
+        plt.show()
+grayimages = [cv2.cvtColor(image, cv2.cv2.COLOR_BGR2GRAY) for image in images]
+lineImages = imageGradientGray(grayimages)
+for image in lineImages:
+    plt.imshow(image, cmap='bone')
+    plt.colorbar()
+    plt.show()
 
 print("part 3")
 
 for image in images:
     getHoughTransform(image)
 
-# TODO
+# TODO later
 #     1. make stuff faster, like list comprehension while sorting and stuff
 #     2. Find ways to make get lines faster by using numpy
 #     3. Make Part 2 better by having to see on all the lines in vicinity of an angle
